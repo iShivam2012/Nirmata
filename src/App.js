6a79b317299a69705f14442d8d79448b43b975a6
iShivam2012 <shivam.gupta97a@gmail.com>
2023-07-13 23:13:18 +0530

    Getting the data from get-players.js and passing to Players component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import getPlayers from "./get-players";
+import Players from "./Components/Players";
+
 function App() {
-  return <div></div>;
+  const [playersData, setPlayersData] = useState([]);
+
+  useEffect(() => {
+    async function playersData() {
+      const data = await getPlayers();
+      setPlayersData(data);
+    }
+    playersData();
+  }, []);
+
+  return (
+    <div>
+      <Players data={playersData} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/Players.js
@@ -0,0 +1,3 @@
+const Players = ({ data }) => {};
+
+export default Players;
