8f3aa23b8e3c693e151cda10a5f296a0969230ee
iShivam2012 <shivam.gupta97a@gmail.com>
2023-07-14 03:15:52 +0530

    implemented sorting functionality which is sorting in both asc and desc order

--- src/Components/Players.js
+++ src/Components/Players.js
@@ -4,11 +4,16 @@ import PageNavigation from "./PageNavigation";
 
 const Players = ({ data }) => {
   const [currentPage, setCurrentPage] = useState(1);
+  const playersData = data.map((pData) => {
+    const age = new Date().getFullYear() - new Date(pData.dob).getFullYear();
+    const dob = new Date(pData.dob).toDateString();
+    return { ...pData, age: age, dob: dob };
+  });
   const recordsPerPage = 10;
   const lastIndex = currentPage * recordsPerPage;
   const firstIndex = lastIndex - recordsPerPage;
-  const records = data.slice(firstIndex, lastIndex);
-  const totalNoOfPages = Math.ceil(data.length / recordsPerPage);
+  const records = playersData.slice(firstIndex, lastIndex);
+  const totalNoOfPages = Math.ceil(playersData.length / recordsPerPage);
   const numbers = [...Array(totalNoOfPages + 1).keys()].slice(1);
 
   const onPrevPageChangeHandler = () => {
@@ -29,7 +34,7 @@ const Players = ({ data }) => {
 
   return (
     <div>
-      <Records records={records} data={data} />
+      <Records currentPage={currentPage} records={records} data={playersData} />
       <PageNavigation
         numbers={numbers}
         onNextPageChangeHandler={onNextPageChangeHandler}
--- src/Components/Records.js
+++ src/Components/Records.js
@@ -1,22 +1,65 @@
-const Records = ({ data, records }) => {
+import { useEffect, useState } from "react";
+
+const Records = ({ data, records, currentPage }) => {
+  const [sortedRecords, setSortedRecords] = useState(records);
+  const [order, setOrder] = useState("ASC");
+  const sortHandler = (e) => {
+    const property = e.target.innerText;
+    if (property === "Name") {
+      if (order === "ASC") {
+        const sd = [...records].sort((a, b) => (a.name > b.name ? 1 : -1));
+        setSortedRecords(sd);
+        setOrder("DSC");
+      }
+      if (order === "DSC") {
+        const sd = [...records].sort((a, b) => (b.name > a.name ? 1 : -1));
+        setSortedRecords(sd);
+        setOrder("ASC");
+      }
+    } else if (property === "Rank") {
+      if (order === "ASC") {
+        const sd = [...records].sort((a, b) => (a.rank > b.rank ? 1 : -1));
+        setSortedRecords(sd);
+        setOrder("DSC");
+      }
+      if (order === "DSC") {
+        const sd = [...records].sort((a, b) => (b.rank > a.rank ? 1 : -1));
+        setSortedRecords(sd);
+        setOrder("ASC");
+      }
+    } else if (property === "Age") {
+      if (order === "ASC") {
+        const sd = [...records].sort((a, b) => (a.age > b.age ? 1 : -1));
+        setSortedRecords(sd);
+        setOrder("DSC");
+      }
+      if (order === "DSC") {
+        const sd = [...records].sort((a, b) => (b.age > a.age ? 1 : -1));
+        setSortedRecords(sd);
+        setOrder("ASC");
+      }
+    }
+  };
+
+  useEffect(() => {
+    setSortedRecords(records);
+  }, [currentPage]);
+
   return (
     <table className="table">
       <thead>
         <tr>
-          <th>Name</th>
+          <th onClick={sortHandler.bind("ASC")}>Name</th>
           <th>Description</th>
           <th>Type</th>
           <th>Points</th>
-          <th>Rank</th>
+          <th onClick={sortHandler.bind("ASC")}>Rank</th>
           <th>Date of Birth</th>
-          <th>Age</th>
+          <th onClick={sortHandler.bind("ASC")}>Age</th>
         </tr>
       </thead>
       <tbody>
-        {records.map((data) => {
-          const dob = new Date(data.dob).toDateString();
-          const age =
-            new Date().getFullYear() - new Date(data.dob).getFullYear();
+        {sortedRecords.map((data) => {
           return (
             <tr key={data.id}>
               <td>{data.name}</td>
@@ -24,8 +67,8 @@ const Records = ({ data, records }) => {
               <td>{data.type}</td>
               <td>{data.points}</td>
               <td>{data.rank}</td>
-              <td>{dob}</td>
-              <td>{age}</td>
+              <td>{data.dob}</td>
+              <td>{data.age}</td>
             </tr>
           );
         })}
